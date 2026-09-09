7b3b7d56e42caf6c3c91ba1cc14213f9ff43feb9
Justin Ficca <jaf330@lehigh.edu>
2026-09-09 15:20:23 -0400

    Uploaded Program 3 sept 3

---
+++ Programming Activities/Program3.py
@@ -0,0 +1,5 @@
+#This is the Revision of Program 2 to save my name stored with a variable
+#variable to store my name
+name = 'Justin Ficca'
+
+print(name) #output the contents pf the variable name to the REPL
